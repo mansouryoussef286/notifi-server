93af7580b97857e2e6875e009c6c6244be6ec37f
Youssef Mansour <mansouryoussef286@gmail.com>
2024-05-20 13:53:46 +0300

    add env configuration service validations

--- src/Config/App.Config.ts
+++ src/Config/App.Config.ts
@@ -1,9 +1,14 @@
 import { Inject, Injectable } from '@nestjs/common';
 import { ConfigType } from '@nestjs/config';
 import Configuration from '@App/Config/Configuration';
-import { IsString, ValidateNested } from 'class-validator';
+import {
+  IsString,
+  ValidateNested,
+  ValidationError,
+  validate,
+} from 'class-validator';
 import { Server } from './Types';
-import { Type } from 'class-transformer';
+import { Type, plainToClass } from 'class-transformer';
 
 export class Config {
   @IsString()
@@ -15,7 +20,7 @@ export class Config {
   @ValidateNested()
   @Type(() => Server)
   Server: Server;
-  
+
   Database: {
     Host: string;
     Name: string;
@@ -43,5 +48,56 @@ export class AppConfig {
   constructor(
     @Inject(Configuration.KEY)
     public Config: ConfigType<typeof Configuration>,
-  ) {}
+  ) {
+    this.validateConfig();
+  }
+
+  async validateConfig() {
+    const configDto = plainToClass(Config, this.Config);
+    try {
+      const errors: ValidationError[] = await validate(configDto);
+      if (errors.length > 0) {
+        console.error('Validation errors in the .env file:');
+        this.displayValidationErrors(errors);
+        process.exit(1); // Terminate the application if configuration is not valid
+      }
+    } catch (e) {
+      console.error('An error occurred during validation:', e);
+      process.exit(1); // Terminate the application if an error occurs
+    }
+  }
+
+  displayValidationErrors(errors: ValidationError[]) {
+    // console.log(errors);
+
+    errors.forEach((error) => {
+      let errorText: string = `${error.property} is not valid:`;
+
+      // error constraints
+      Object.values(error.constraints ?? {}).forEach((constraint) => {
+        errorText += `\n ${constraint}`;
+      });
+
+      // property children
+      error.children?.forEach((child) => {
+        errorText += handleChildren(child);
+      });
+
+      // log the errors
+      console.log('\n' + errorText);
+    });
+
+    function handleChildren(child: ValidationError): string {
+      let retval = '';
+      Object.values(child.constraints ?? {}).forEach((constraint) => {
+        retval += `\n ${constraint}`;
+      });
+
+      // recurse for nested children
+      child.children?.forEach((child) => {
+        retval += handleChildren(child);
+      });
+      return retval;
+    }
+  }
 }
--- src/Config/Types.ts
+++ src/Config/Types.ts
@@ -1,9 +1,10 @@
-import { IsNumber, IsString } from 'class-validator';
+import { Type } from 'class-transformer';
+import { IsNumber, IsString, ValidateNested } from 'class-validator';
 
 export class Server {
   @IsString()
   Host: string;
-  
+
   @IsNumber()
   Port: number;
 }
